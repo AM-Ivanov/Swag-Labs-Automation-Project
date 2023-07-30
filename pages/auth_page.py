dcea2739b273c774760aa59ead199ceaab107dfd
AM-Ivanov <this.teemo.baby@gmaail.com>
2023-07-30 16:38:28 +0200

    Refactoring and improvements

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,6 +1,6 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from base.wd_setup import WebDriverSetup
+from base.input_field_steps import InputFieldSteps
 
 
 class AuthPage(BasePage):
@@ -13,15 +13,10 @@ class AuthPage(BasePage):
             'PASSWORD_FIELD': '//input[@id="password"]',
             'LOGIN_BUTTON': '//input[@id="login-button"]',
         }
-        self.failed_auth_element = {'FAILED_LOGIN_BANNER': '//div[@class="error-message-container error"]',
-                                    'FAILED_LOGIN_TEXT': '//h3[@data-test="error"]',
-                                    'FAILED_LOGIN_BUTTON': '//button[@class="error-button"]',
-                                    'FAILED_LOGIN_USERNAME_FIELD_ICON': '//input[@id="user-name"]/following-sibling::*['
-                                                                        '@data-icon="times-circle"]',
-                                    'FAILED_LOGIN_PASSWORD_FIELD_ICON': '//input[@id="password"]/following-sibling::*['
-                                                                        '@data-icon="times-circle"]'}
-
-
-driver = WebDriverSetup('Chrome', ['detach']).configure()
-cur_page = AuthPage(driver)
-cur_page.find_all_elements()
+        self.failed_auth_elements = {'FAILED_LOGIN_BANNER': '//div[@class="error-message-container error"]',
+                                     'FAILED_LOGIN_TEXT': '//h3[@data-test="error"]',
+                                     'FAILED_LOGIN_BUTTON': '//button[@class="error-button"]',
+                                     'FAILED_LOGIN_USERNAME_FIELD_ICON': '//input[@id="user-name"]/following-sibling::*['
+                                                                         '@data-icon="times-circle"]',
+                                     'FAILED_LOGIN_PASSWORD_FIELD_ICON': '//input[@id="password"]/following-sibling::*['
+                                                                         '@data-icon="times-circle"]'}
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,17 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage:
-    def __init__(self, driver, url):
+    def __init__(self, driver: WebDriver, url):
         self.driver = driver
         self.url = url
         self.driver.get(self.url)
-        self.elements = {}
 
-    def find_all_elements(self):
+    def elements_are_on_page(self, elements):
         self.driver.implicitly_wait(4)
-        for element in self.elements:
+        for element in elements:
             try:
-                self.driver.find_element(By.XPATH, self.elements[element])
+                self.driver.find_element(By.XPATH, elements[element])
             except:
                 print(f'Failed to find element "{element}"')
